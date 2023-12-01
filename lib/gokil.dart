8285cb20556d85c094e12c005288644452645f4c
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2023-12-01 23:30:42 +0700

    fix: export text extension

--- lib/gokil.dart
+++ lib/gokil.dart
@@ -2,6 +2,7 @@ library gokil;
 
 export 'src/extensions/button_extension.dart';
 export 'src/extensions/theme_context.dart';
+export 'src/extensions/text_extension.dart';
 export 'src/provider/theme_controller.dart';
 export 'src/themes/dark_theme.dart';
 export 'src/themes/light_theme.dart';
